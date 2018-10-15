11f0ced957039cc6506b9621367661c51f30f31d
Josenildo Costa <josenildocosta@gmail.com>
2018-10-14 22:23:25 -0300

    Exportar em pdf com pdfmaker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.getUsuarios();
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
   }
 
   constructor(private usuarioService: UsuarioService) { }
@@ -26,38 +27,63 @@ export class AppComponent {
   }
 
   print() {
-    this.usuarioArray = [];
-    this.getUsuarios();
-    console.log(this.usuarioList);
+    this.usuarioArray = [];    
 
+    this.usuarioArray.push([{text: 'Nome',margin: [15, 15, 0, 0],bold: true}]);
     this.usuarioList.forEach(element => {
-      this.usuarioArray.push(element.nome)
+      this.usuarioArray.push([{text: element.nome,margin: [10, 0, 0, 0]}])
     });
 
-    console.log(this.usuarioArray);
-    //called first time before the ngOnInit()
-    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    pdfMake.tableLayouts = {
+      exampleLayout: {
+        hLineWidth: function (i, node) {
+          if (i === 0 || i === node.table.body.length) {
+            return 0;
+          }
+          return (i === node.table.headerRows) ? 2 : 1;
+        },
+        vLineWidth: function (i) {
+          return 0;
+        },
+        hLineColor: function (i) {
+          return i === 1 ? 'black' : '#aaa';
+        },
+        paddingLeft: function (i) {
+          return i === 0 ? 0 : 8;
+        },
+        paddingRight: function (i, node) {
+          return (i === node.table.widths.length - 1) ? 0 : 8;
+        }
+      }
+    };
 
     var dd = {
+      header: {
+        text: 'Lista de ramais',
+        alignment: 'center',
+        margin: [0, 15, 0, 20],
+        fontSize: 18,
+        bold: true
+      },      
+      footer: function(currentPage, pageCount) { 
+        return {
+          text: currentPage.toString() + ' de ' + pageCount,
+          alignment: 'center',
+          margin: [0, 15, 0, 0]
+        }; 
+      },
       content: [
         {
           layout: 'lightHorizontalLines', // optional
           table: {
-            // headers are automatically repeated if the table spans over multiple pages
-            // you can declare how many rows should be treated as headers
             headerRows: 1,
-            widths: ['*', 'auto', 100, '*'],
+            widths: ['*'],
 
-            body: [
-              ['First', 'Second', 'Third', 'The last one'],
-              this.usuarioArray,
-              [{ text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4']
-            ]
+            body: this.usuarioArray
           }
         }
       ]
     };
     pdfMake.createPdf(dd).download();
   }
-
 }
